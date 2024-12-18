e45d155c77133b48efd02c708dc628bb5bdd51b6
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-18 13:36:40 +0100

    Update app.py

--- src/jarsigner_gui/app.py
+++ src/jarsigner_gui/app.py
@@ -6,6 +6,7 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 import asyncio
+import subprocess
 
 
 class JarSigner(toga.App):
@@ -17,27 +18,24 @@ class JarSigner(toga.App):
         show the main window.
         """
         self.keystore_label = toga.Label("No keyfiles selected...")
-        self.apk_label = toga.Label("No apkfiles selected...")
+        self.android_label = toga.Label("No apkfiles selected...")
         self.select_key = toga.Button(
             "Select Key",
             on_press=self.key_action_open_file_dialog,
         )
-        self.apk_file = toga.Button(
+        self.android_file = toga.Button(
             "Select APK file",
-            on_press=self.apk_action_open_file_dialog,
+            on_press=self.android_action_open_file_dialog,
         )
-        self.cmd = "jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore my_application.apk alias_name"
-        self.cmd = self.cmd.split(" ")
-        print(self.cmd)
         self.alias_name = toga.TextInput(placeholder="Alias")
         self.password = toga.PasswordInput(placeholder="Password")
         signbtn = toga.Button("Sign")
         main_box = toga.Box(
             children=[
                 self.keystore_label,
-                self.apk_label,
+                self.android_label,
                 self.select_key,
-                self.apk_file,
+                self.android_file,
                 self.alias_name,
                 self.password,
                 signbtn,
@@ -48,18 +46,18 @@ class JarSigner(toga.App):
         self.main_window.content = main_box
         self.main_window.show()
 
-    async def apk_action_open_file_dialog(self, widget):
+    async def android_action_open_file_dialog(self, widget):
         try:
-            self.apkfile = await self.main_window.dialog(
-                toga.OpenFileDialog("Open file with Toga", file_types=["apk"])
+            self.androidfile = await self.main_window.dialog(
+                toga.OpenFileDialog("Open file with Toga", file_types=["apk", "aab"])
             )
-            print(self.apkfile)
-            if self.apkfile is not None:
-                self.apk_label.text = f"File to open: {self.apkfile}"
+            print(self.androidfile)
+            if self.androidfile is not None:
+                self.android_label.text = f"File to open: {self.androidfile}"
             else:
-                self.apk_label.text = "No apkfiles selected..."
+                self.android_label.text = "No apkfiles selected..."
         except ValueError:
-            self.apk_label.text = "Open file dialog was canceled"
+            self.android_label.text = "Open file dialog was canceled"
 
     async def key_action_open_file_dialog(self, widget):
         try:
@@ -73,6 +71,15 @@ class JarSigner(toga.App):
                 self.keystore_label.text = "No keyfiles selected..."
         except ValueError:
             self.keystore_label.text = "Open file dialog was canceled"
+
+    def sign(self):
+        try:
+            self.cmd = f"jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore {self.keyfile} {self.android_file} {self.alias_name}"
+            self.cmd = self.cmd.split(" ")
+            subprocess.run(self.cmd)
+            print("ready")
+        except:
+            print("Fail.")
 
 
 def main():
