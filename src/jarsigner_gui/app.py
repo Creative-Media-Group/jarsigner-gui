99c1b28ef6a54c1db1ee9c375870c6f959aa2d2e
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-18 13:18:22 +0100

    Update app.py

--- src/jarsigner_gui/app.py
+++ src/jarsigner_gui/app.py
@@ -16,8 +16,8 @@ class JarSigner(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        # jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore my_application.apk alias_name
-        self.keystore_label = toga.Label("No files selected...")
+        self.keystore_label = toga.Label("No keyfiles selected...")
+        self.apk_label = toga.Label("No apkfiles selected...")
         self.select_key = toga.Button(
             "Select Key",
             on_press=self.key_action_open_file_dialog,
@@ -26,12 +26,16 @@ class JarSigner(toga.App):
             "Select APK file",
             on_press=self.apk_action_open_file_dialog,
         )
+        self.cmd = "jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore my_application.apk alias_name"
+        self.cmd = self.cmd.split(" ")
+        print(self.cmd)
         self.alias_name = toga.TextInput(placeholder="Alias")
         self.password = toga.PasswordInput(placeholder="Password")
         signbtn = toga.Button("Sign")
         main_box = toga.Box(
             children=[
                 self.keystore_label,
+                self.apk_label,
                 self.select_key,
                 self.apk_file,
                 self.alias_name,
@@ -51,23 +55,24 @@ class JarSigner(toga.App):
             )
             print(self.apkfile)
             if self.apkfile is not None:
-                self.keystore_label = f"File to open: {self.apkfile}"
+                self.apk_label.text = f"File to open: {self.apkfile}"
             else:
-                self.keystore_label = "No files selected..."
+                self.apk_label.text = "No apkfiles selected..."
         except ValueError:
-            self.keystore_label = "Open file dialog was canceled"
+            self.apk_label.text = "Open file dialog was canceled"
 
     async def key_action_open_file_dialog(self, widget):
         try:
             self.keyfile = await self.main_window.dialog(
                 toga.OpenFileDialog("Open file with Toga", file_types=["keystore"])
             )
+            print(self.keyfile)
             if self.keyfile is not None:
-                self.keystore_label = f"File to open: {self.keyfile}"
+                self.keystore_label.text = f"File to open: {self.keyfile}"
             else:
-                self.keystore_label = "No file selected!"
+                self.keystore_label.text = "No keyfiles selected..."
         except ValueError:
-            self.keystore_label = "Open file dialog was canceled"
+            self.keystore_label.text = "Open file dialog was canceled"
 
 
 def main():
